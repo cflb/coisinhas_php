6af680b87945e82ad1417b7c70bebcd407b0445f
cflb <50557392+cflb@users.noreply.github.com>
2020-03-05 10:03:10 -0300

    Create array.php

---
+++ array.php
@@ -0,0 +1,21 @@
+<?php
+
+$est1 = array(
+    'nome' => 'Joao',
+);
+
+$est2 = array(
+    'nome' => 'pedro',
+);
+
+$est3 = array(
+    'nome' => 'Ana Julia',
+);
+
+$estudantes = array(
+    $est1, $est2, $est3
+);
+
+print_r($estudantes);
+
+?>
